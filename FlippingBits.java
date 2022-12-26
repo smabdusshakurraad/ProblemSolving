6072f298a0ab5bea43496d80866495a3dcafa963
Abdus Sakur <abdusshakurraad@gmail.com>
2022-12-27 00:39:37 +0600

    week 2 solves

---
+++ FlippingBits.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+/*
+problem link:
+https://www.hackerrank.com/challenges/three-month-preparation-kit-flipping-bits/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+ */
+public class FlippingBits {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = FlippingBitsResult.flippingBits(n);
+
+                System.out.println(result);
+
+                // bufferedWriter.write(String.valueOf(result));
+                //     bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        // bufferedWriter.close();
+    }
+}
+class FlippingBitsResult{
+
+    /*
+     * Complete the 'flippingBits' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long flippingBits(long n) {
+        // Write your code here
+        return (~n & 0xffffffffL);
+    }
+
+}
