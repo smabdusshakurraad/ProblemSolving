1d6fd962b85a5df01c140a44ca97f7d41217befd
Abdus Sakur <abdusshakurraad@gmail.com>
2022-12-31 01:49:29 +0600

    week 2 solves

---
+++ CountingSortOne.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/*
+problem link:
+https://www.hackerrank.com/challenges/three-month-preparation-kit-countingsort1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-two
+ */
+
+public class CountingSortOne {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = CountingSortOneResult.countingSort(arr);
+
+//        bufferedWriter.write(
+//                result.stream()
+//                        .map(Object::toString)
+//                        .collect(joining(" "))
+//                        + "\n"
+//        );
+
+        bufferedReader.close();
+        System.out.println(result);
+//        bufferedWriter.close();
+    }
+}
+class CountingSortOneResult{
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        // Write your code here
+        int[] array = new int[100];
+        List<Integer> arrayList = new ArrayList<>(100);
+        for (Integer integer : arr) {
+            array[integer]++;
+        }
+        for(int i = 0;i<100;i++){
+            arrayList.add(i,array[i]);
+        }
+        return arrayList;
+    }
+}
