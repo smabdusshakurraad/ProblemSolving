04b94653736528d02e06f7a5e92abe3ebca1f04c
Abdus Sakur <abdusshakurraad@gmail.com>
2023-01-03 11:16:22 +0600

    week 2 solves

---
+++ FlippingTheMatrix.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Moc test week 2
+ */
+public class FlippingTheMatrix {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<List<Integer>> matrix = new ArrayList<>();
+
+                IntStream.range(0, 2 * n).forEach(i -> {
+                    try {
+                        matrix.add(
+                                Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                        .map(Integer::parseInt)
+                                        .collect(toList())
+                        );
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                int result = FlippingTheMatrixResult.flippingMatrix(matrix);
+//
+//                bufferedWriter.write(String.valueOf(result));
+//                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+//        bufferedWriter.close();
+    }
+}
+class FlippingTheMatrixResult{
+    /*
+     * Complete the 'flippingMatrix' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+     */
+
+    public static int flippingMatrix(List<List<Integer>> matrix) {
+        // Write your code here
+        int size = matrix.size();
+        int sum =0;
+        for(int i=0;i<size/2;i++){
+            for(int j=0;j<size/2;j++){
+                sum += Math.max(matrix.get(i).get(j),
+                        Math.max(matrix.get(i).get(size-1-j),
+                                Math.max(matrix.get(size-1-i).get(j),
+                                        matrix.get(size-1-i).get(size-1-j))));
+            }
+        }
+
+        return sum;
+
+    }
+}
