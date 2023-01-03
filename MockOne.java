769969f32efaa953aaa4aa92f6bbc8ced77a95a0
Abdus Sakur <abdusshakurraad@gmail.com>
2023-01-03 19:53:48 +0600

    week 3 solves

--- MockOne.java
+++ MockOne.java
@@ -1,2 +1,45 @@
 public class MockOne {
+    public static void main(String[] args){
+        int[] arr = {3,6,1,5,4};
+        int n = arr.length;
+        int m = median(n,arr);
+
+        System.out.println(m);
+
+        for (int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    private static int median(int n, int[] arr) {
+        quickSort(arr,0,n-1);
+        return arr[n/2];
+    }
+
+    private static int partition(int[] arr, int start, int end){
+        int pivot = arr[end];
+        int i = start-1;
+        int temp;
+        for(int j=start;j<end;j++){
+            if(arr[j]<pivot){
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        temp = arr[i];
+        arr[i] = arr[end];
+        arr[end] = temp;
+
+        return i;
+    }
+    private static void quickSort(int[] arr, int start, int end) {
+        if(start<end){
+            int pivotIndx = partition(arr,start,end);
+            quickSort(arr,start,pivotIndx-1);
+            quickSort(arr,pivotIndx+1,end);
+        }
+    }
 }
---
+++ SubArrayDivisionTwo.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class SubArrayDivisionTwo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = SubArrayDivisionTwoResult.birthday(s, d, m);
+        System.out.println(result);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+
+        bufferedReader.close();
+//        bufferedWriter.close();
+    }
+}
+class SubArrayDivisionTwoResult{
+
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<Integer> s, int d, int m) {
+        // Write your code here
+        int sum= 0, count = 0;
+        if(s.size()<m){
+            return count;
+        }
+        else if(s.size()>m){
+            for(int i=0;i<s.size()-m;i++){
+                sum = 0;
+                for(int j =0;j<m;j++){
+                    sum += s.get(i+j);
+                }
+                if(sum==d){
+                    count++;
+                }
+            }
+        }else{
+            for(int i=0;i<m;i++){
+                sum += s.get(i);
+            }
+            if(sum ==d){
+                count++;
+            }
+        }
+
+
+        return count;
+
+    }
+}
